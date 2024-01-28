ca05b4ef60c6d7d2790b0acc55e2e4dedb13ef95
Jan Górski <77403638+JanG175@users.noreply.github.com>
2024-01-28 15:44:15 +0100

    Update vl53l0x.h

--- include/vl53l0x.h
+++ include/vl53l0x.h
@@ -1,3 +1,11 @@
+/**
+ * @file vl53l0x.h
+ * @author JanG175
+ * @brief ESP-IDF component for VL53L0X ToF distance sensor
+ * 
+ * @copyright Apache 2.0
+*/
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
